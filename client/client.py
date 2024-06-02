3390b0f14a138e24a05e37721e4418f30be6d0ac
rlazoc <49696401+rlazoc@users.noreply.github.com>
2024-06-02 09:46:15 -0500

    netdog
    add simple logging

--- client/client.py
+++ client/client.py
@@ -1,17 +1,22 @@
 import socket
+import logging
 
 def start_client(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((host, port))
-        print(f"Connected to {host}:{port}")
-        
-        while True:
-            message = input('Enter message: ')
-            if message.lower() == 'exit':
-                break
-            client_socket.sendall(message.encode('utf-8'))
-            data = client_socket.recv(1024)
-            print(f"Received: {data.decode('utf-8')}")
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((host, port))
+            print(f"Connected to {host}:{port}")
+
+            while True:
+                message = input('Enter message: ')
+                if message.lower() == 'exit':
+                    break
+                client_socket.sendall(message.encode('utf-8'))
+                data = client_socket.recv(1024)
+                print(f"Received: {data.decode('utf-8')}")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     start_client('127.0.0.1', 9999)
--- server/server.py
+++ server/server.py
@@ -1,20 +1,25 @@
 import socket
+import logging
 
 def start_server(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((host, port))
-        server_socket.listen(1)
-        print(f"Listening on {host}: {port}")
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((host, port))
+            server_socket.listen(1)
+            logging.info(f"Listening on {host}:{port}")
 
-        conn, addr = server_socket.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(f"Recieved: {data.decode('utf-8')}")
-                conn.sendall(data)
+            conn, addr = server_socket.accept()
+            with conn:
+                logging.info(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    logging.info(f"Received: {data.decode('utf-8')}")
+                    conn.sendall(data)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     start_server('0.0.0.0', 9999)
